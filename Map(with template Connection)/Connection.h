7ee1b97556b2f3cf75f0704a553a38ce78a76a4b
15600800736 <183425990@qq.com>
2016-10-16 11:35:52 +0800

    Connection
    the child class of Relation,defined as two node's relationship

--- Map(with template Connection)/Connection.h
+++ Map(with template Connection)/Connection.h
@@ -7,16 +7,85 @@
 
 namespace huger
 {
+
+/*
+ *
+ *	The child class of Relation specially for two node's connection
+ *	The member value means two node's distance
+ *	more specific details should be defined in node's class
+ *	The connection equals one another only when they have the same source destination and distance
+ *	Of course,if source and destination equals,distance must be equal
+ *
+ */
 template<typename Object>
-class Connection :public Relation<Object,int>
+class Connection :public Relation<Object,Object,int>
 {
 public:
-	typedef Relation::dataType dataType;
-	typedef Relation::object object;
+	typedef Relation::valueType distanceTypeType;
+	typedef Relation::firstType objectType;
 
+	///////////////////////////////////////////////////////////////////////
+	//	-constructor
+	Connection()
+	{
 
-protected:
-	
+	}
+	Connection(objectType* sourceNode,
+		objectType* destinationNode,
+		distanceType distance) :
+		_firstObject(sourceNode),
+		_secondObject(destinationNode),
+		_value(distance)
+	{
+
+	}
+	///////////////////////////////////////////////////////////////////////
+	//	-overload operator==
+	//	 The connection equals one another only when they have the same source destination and distance
+	//	Of course, if source and destination equals, distance must be equal
+	//	@parameter otherConnection - the source
+	bool operator==(const Connection& otherConnection)
+	{
+		return	_firstObject == otherConnection._firstObject && 
+			_secondObject == otherConnection._secondObject && 
+			_data == otherConnection._data;
+	}	
+	///////////////////////////////////////////////////////////////////////
+	//	-overload operator!=
+	//	-As long as one of them dosen't equal
+	//	-They don't equal
+	//	@parameter otherConnection - the source
+	bool operator!=(const Connection& otherConnection)
+	{
+		return	_firstObject != otherConnection._firstObject ||
+			_secondObject != otherConnection._secondObject ||
+			_data != otherConnection._data;
+	}
+	///////////////////////////////////////////////////////////////////////
+	//	-get the node connected to
+	objectType getDestination()const
+	{
+		return _secondObject;
+	}
+	///////////////////////////////////////////////////////////////////////
+	//	-get the node connected from
+	objectType getSource()const
+	{
+		return _firstObject;
+	}	
+	///////////////////////////////////////////////////////////////////////
+	//	-get two node's distance
+	distanceType getDistance()const
+	{
+		return _value;
+	}
+	///////////////////////////////////////////////////////////////////////
+	//	-set two node's distance
+	//	@parameter value - source
+	void setDistance(distanceType value)
+	{
+		_value = value;
+	}
 };
 }
 #endif
--- Map(with template Connection)/Relation.h
+++ Map(with template Connection)/Relation.h
@@ -4,37 +4,44 @@
 
 #ifndef RELATION_H
 #define RELATION_H
+
+#include "global.h"
 namespace huger
 {
 /*
  *
  *	Abstract class of Relation
  *	This class is the base-class of all kinds of relationship
- *	Undefine function to get objects and data because maybe the child classes don't want to get both of them
+ *	Undefine function to get objects and value because maybe the child classes don't want to get both of them
  *	Or they need different function name
  *
  */
-template<typename Object,typename DataType>
+template<typename First,typename Second,typename DataType>
 class Relation
 {
 public:
-	typedef typename DataType dataType;
-	typedef typename Object object;
+	typedef typename DataType valueType;
+	typedef typename First firstType;
+	typedef typename Second secondType;
 	//////////////////////////////////////////////////////////////////////////////
 	//	-constructor
+	Relation() :_firstObject(NULL), _secondObject(NULL), _value(NULL)
+	{
+
+	}
 	Relation(object* firstOjbect, 
 		object* secondOjbect,
-		dataType data) :
+		valueType value) :
 		_firstOjbect(firstOjbect), 
 		_secondObject(secondOjbect),
-		_data(data)
+		_value(value)
 	{
 	}
 	Relation(const Relation& other)
 	{
 		_firstObject = other._firstObject;
 		_secondObject = other._secondObject;
-		_data = other._data;
+		_value = other._value;
 	}
 	virtual ~Relation()
 	{
@@ -46,9 +53,9 @@ public:
 	//	-overlaoding operator!=
 	virtual bool operater != (const Relation& otherRelation) = 0;
 protected:
-	object* _firstObject;
-	object* _secondObject;
-	dataType _data;
+	firstType* _firstObject;
+	secondType* _secondObject;
+	valueType _value;
 };
 }
 #endif
