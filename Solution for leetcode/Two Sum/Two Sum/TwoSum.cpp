384737af6aed78f177a42d073d7ee7bea44629e4
15600800736 <183425990@qq.com>
2016-11-04 08:27:19 +0800

    sort and lower_bound

--- Solution for leetcode/Two Sum/Two Sum/TwoSum.cpp
+++ Solution for leetcode/Two Sum/Two Sum/TwoSum.cpp
@@ -10,26 +10,32 @@
 class Solution {
 public:
 	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		bool first = false, second = false;
 		std::vector<int> sortedNums(nums);
 		std::vector<int> result;
 		std::vector<int>::iterator begin = sortedNums.begin();
 		std::vector <int>::iterator end = sortedNums.end();
 		std::sort(begin, end);
 		std::vector<int>::iterator iter = sortedNums.begin();
-		std::vector<int>::iterator iterTarget = sortedNums.end();
-		while(iter != end){
-			iterTarget = std::lower_bound(iter + 1, sortedNums.end(), target - *iter);
+		std::vector<int>::iterator iterTarget;
+		while (iter != sortedNums.end()) {
+			iterTarget = std::lower_bound(iter + 1, end, target - *iter);
 			if (iterTarget != sortedNums.end()) {
-				break;
+				if (*iterTarget + *iter == target)
+					break;
 			}
 			iter++;
 		}
 		for (int i = 0; i < nums.size(); i++) {
-			if (nums[i] == *iter) {
+			if (nums[i] == *iter && !first) {
 				result.push_back(i);
+				first = true;
+			} else if (nums[i] == *iterTarget && !second) {
+				result.push_back(i);
+				second = true;
 			}
-			if (nums[i] == *iterTarget) {
-				result.push_back(i);
+			if (first && second) {
+				break;
 			}
 		}
 		return result;
